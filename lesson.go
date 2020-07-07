840329a24a18e6fb35ce1e57891af7ff2124999a
ei1125 <i.dont.like.monday1125@gmail.com>
2020-07-07 11:06:51 +0900

    practice-basic-1-12

---
+++ lesson.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// initは一番始めに呼び出される
+/*
+func init() {
+	fmt.Println("Init!")
+}
+*/
+
+func bazz() {
+	fmt.Println("Bazz")
+}
+
+// func main() {
+// 	//bazz()
+// 	fmt.Println("Hello world!")
+// }
